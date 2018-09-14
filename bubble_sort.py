5d80bf204238017c52a76497663544e5605aec55
Varun Reddy <varunreddy1061@gmail.com>
2018-09-14 18:52:54 +0530

    bubble sort implementation

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        for j in range(0,n-1):
+            if(arr[j+1]<arr[j]):
+                temp=arr[j+1]
+                arr[j+1]=arr[j]
+                arr[j]=temp
+    return arr
+print(bubble_sort([7,2,4,1,5,3,0,-1,-2]))
