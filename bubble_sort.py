46cb8519b48a341d83b95da7858727dac9b5f6a6
Varun Reddy <varunreddy1061@gmail.com>
2018-09-14 19:14:22 +0530

    added flag to reduce iterations

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,14 @@
 def bubble_sort(arr):
     n=len(arr)
     for i in range(0,n-1):
-        for j in range(0,n-1):
+        flag=0
+        for j in range(0,n-i-1):
             if(arr[j+1]<arr[j]):
                 temp=arr[j+1]
                 arr[j+1]=arr[j]
                 arr[j]=temp
+                flag=1
+        if(flag==0):
+            break
     return arr
-print(bubble_sort([7,2,4,1,5,3,0,-1,-2]))
+print(bubble_sort([7,2,4,1,5,3]))
