78d3649fa9d23d054683b58193af20a7cab7eec4
Varun Reddy <varunreddy1061@gmail.com>
2018-09-14 18:56:57 +0530

    changed the index of first loop

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,6 @@
 def bubble_sort(arr):
     n=len(arr)
-    for i in range(1,n):
+    for i in range(0,n-1):
         for j in range(0,n-1):
             if(arr[j+1]<arr[j]):
                 temp=arr[j+1]
