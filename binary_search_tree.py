5b35701d7bc007ecb062fa16b0639cfc49b0c844
Varun Reddy <varunreddy1061@gmail.com>
2018-10-23 02:59:57 -0400

    Pre-order implementation

---
+++ binary_search_tree.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+class BinaryTree(object):
+    def __init__(self,root):
+        self.root = Node(root)
+    
+    def preorder_print(self, start, traversal):
+        """Root -> Left -> Right"""
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
