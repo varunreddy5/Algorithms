6fa6a74357f479640107907ff69de27f7d11131f
Varun Reddy <varunreddy1061@gmail.com>
2018-11-05 05:22:54 -0500

    Size of the binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -86,6 +86,44 @@ class BinaryTree:
         left_height = self.height(node.left)
         right_height = self.height(node.right)
         return 1+max(left_height, right_height)
+    def height_iterative(self, node):
+        queue = []
+        queue.insert(0, node)
+        height = -1
+        result = []
+        while queue:
+            length_queue = len(queue)
+            while length_queue > 0:
+                node = queue.pop()
+                if node.left:
+                    queue.insert(0, node.left)
+                if node.right:
+                    queue.insert(0, node.right)
+                length_queue-=1
+            height += 1
+        return height
+    def size_iterative(self):
+        if self.root is None:
+            return 0
+        stack = []
+        size = 1
+        stack.append(self.root)
+        while stack:
+            node = stack.pop()
+            if node.left:
+                size+=1
+                stack.append(node.left)
+            if node.right:
+                size+=1
+                stack.append(node.right)
+        return size
+    def size_recursive(self, node):
+        if node is None:
+            return 0
+        return 1  + self.size_recursive(node.left) + self.size_recursive(node.right)
+
+
+
 
 
 #Set up the tree
@@ -94,7 +132,10 @@ tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
-tree.root.right.left = Node(6)
-tree.root.right.right = Node(7)
+# tree.root.right.left = Node(6)
+# tree.root.right.right = Node(7)
 print(tree.print_tree("levelorder"))
 print(tree.height(tree.root))
+print(tree.height_iterative(tree.root))
+print(tree.size_iterative())
+print(tree.size_recursive(tree.root))
