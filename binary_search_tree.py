e833c74485c892fa8f56aa13c5409bad7a0a57c3
Varun Reddy <varunreddy1061@gmail.com>
2018-11-05 06:06:46 -0500

    Insertion and Search Operations in Binary Search Tree

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,25 +1,51 @@
 class Node:
-    def __init__(self,value):
-        self.value = value
+    def __init__(self, data = None):
+        self.data = data
         self.left = None
         self.right = None
-class BinaryTree(object):
-    def __init__(self,root):
-        self.root = Node(root)
-    
-    def preorder_print(self, start, traversal):
-        """Root -> Left -> Right"""
-        if start:
-            traversal += (str(start.value) + "-")
-            traversal = self.preorder_print(start.left, traversal)
-            traversal = self.preorder_print(start.right, traversal)
-        return traversal
+class BST:
+    def __init__(self):
+        self.root = None
 
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            self._insert(data, self.root)
+    def _insert(self, data, cur_node):
+        if data < cur_node.data:
+            if cur_node.left is None:
+                cur_node.left = Node(data)
+            else:
+                self._insert(data, cur_node.left)
+        elif data > cur_node.data:
+            if cur_node.right is None:
+                cur_node.right = Node(data)
+            else:
+                self._insert(data, cur_node.right)
+        else:
+            print("Duplicate values are not allowed")
 
-tree = BinaryTree(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
-tree.root.left.left = Node(4)
-tree.root.left.right = Node(5)
-tree.root.right.left = Node(6)
-tree.root.right.right = Node(7)
+    def find(self, data):
+        if self.root is None:
+            return None
+        else:
+            is_found = self._find(data, self.root)
+            if is_found:
+                return True
+            return False
+    def _find(self, data, cur_node):
+        if data < cur_node.data and cur_node.left:
+            return self._find(data, cur_node.left)
+        elif data > cur_node.data and cur_node.right:
+            return self._find(data, cur_node.right)
+        if data == cur_node.data:
+            return True
+
+bst = BST()
+bst.insert(5)
+bst.insert(4)
+bst.insert(6)
+bst.insert(3)
+bst.insert(7)
+print(bst.find(4))
